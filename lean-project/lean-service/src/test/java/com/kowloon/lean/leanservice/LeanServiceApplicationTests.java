6619c45d14ffe4e7cb33bcdc6c7d8767a5d438df
lean <405830333@qq.com>
2018-10-23 11:59:15 +0800

    提交lean-project

---
+++ lean-project/lean-service/src/test/java/com/kowloon/lean/leanservice/LeanServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kowloon.lean.leanservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeanServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
